650112872085aa7f04744a9b86dc55b29ca08c3d
ScottMo1202 <xingym2@uw.edu>
2020-02-27 20:38:33 -0800

    finished

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let svgContainer = d3.select("body")
                      .attr("width", 1000)
                      .attr("height", 800)
 let neighborhoods = svgContainer.append('g')
-
 d3.json('./data/nygeo.json').then(function(data) {
     let projection = d3.geoAlbers()
             .scale(100000)
@@ -22,30 +21,30 @@ d3.json('./data/nygeo.json').then(function(data) {
      d3.csv('data/data.csv').then((points) => {
              let airbnbs = svgContainer.append("g");
              airbnbs.selectAll('circle')
-                        .data(points)
-                        .enter()
-                        .append('circle')
-                        .attr("class", "dot")
-                        .attr("cx", (d) => {
-                                return projection([d.longitude, d.latitude])[0]
-                        })
-                        .attr("cy", (d) => {
-                                return projection([d.longitude, d.latitude])[1]
-                        })
-                        .attr('fill', '#00FFFF')
-                        .attr('r', 3)
-                        .on('click', function() {
-                                d3.select(this)
-                                  .attr("opacity", 1)
-                                  .transition()
-                                  .duration(500)
-                                  .attr("x", 1000 * Math.round(Math.random()))
-                                  .attr("y", 800 * Math.round(Math.random()))
-                                  .attr("opacity", 0)
-                                  .on("end", () => {
-                                          d3.select(this).remove()
-                                  })
-                        })
+                .data(points)
+                .enter()
+                .append('circle')
+                .attr("class", "dot")
+                .attr("cx", (d) => {
+                        return projection([d.longitude, d.latitude])[0]
+                })
+                .attr("cy", (d) => {
+                        return projection([d.longitude, d.latitude])[1]
+                })
+                .attr('fill', '#00FFFF')
+                .attr('r', 3)
+                .on('click', function() {
+                        d3.select(this)
+                                .attr("opacity", 1)
+                                .transition()
+                                .duration(500)
+                                .attr("x", 1000 * Math.round(Math.random()))
+                                .attr("y", 800 * Math.round(Math.random()))
+                                .attr("opacity", 0)
+                                .on("end", () => {
+                                        d3.select(this).remove()
+                                })
+                })
      })
 
 })
